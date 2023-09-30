e8824a3c814682fa4bf9a649e807e977c0f954ee
Benex Drake <benex@hotmail.de>
2023-09-30 13:59:19 +0200

    changed GET to POST

--- pages/api/crunchyroll/[id].tsx
+++ pages/api/crunchyroll/[id].tsx
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler( req: NextApiRequest, res: NextApiResponse ) 
 {
-    if(req.method === 'GET')
+    if(req.method === 'POST' && req.body.key === process.env.PROTECT_API)
     {
         const animes = await (await GetDB('Crunchyroll','Animes')).find().toArray();
 
