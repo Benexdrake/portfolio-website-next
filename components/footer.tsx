9b1e4593f1e3ffeae9c56136a1037794aa6fb496
Benex Drake <benex@hotmail.de>
2023-10-06 12:48:56 +0200

    deleted footer

--- components/footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer()
-{
-
-
-    return (
-        <div className="fixed-bottom footer">Diese Website wurde mit NextJS erstellt, die neue Version wird unter <a href="https://dev.benrichter.me" target="_blank" style={{textDecoration: 'none', color: 'var(--color3)'}}>https://dev.benrichter.me</a> published</div>
-    )
-}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { Fragment } from 'react'
 import Navbar from '@/components/navbar'
-import Footer from '@/components/footer'
 
 export default function App({ Component, pageProps:{session, ...pageProps} }: AppProps) {
   return (
@@ -12,7 +11,6 @@ export default function App({ Component, pageProps:{session, ...pageProps} }: Ap
       <Fragment>
         <Navbar/>
         <Component {...pageProps} />
-        <Footer/>
       </Fragment>
     </SessionProvider>
   )
