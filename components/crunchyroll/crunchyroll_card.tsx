f3b5bc60c365ece04a4bd0df6b8df12ec1724ac5
Benex Drake <benex@hotmail.de>
2023-10-13 12:31:03 +0200

    crunchyroll cards looking better now

--- components/crunchyroll/crunchyroll_card.tsx
+++ components/crunchyroll/crunchyroll_card.tsx
@@ -1,15 +1,30 @@
 import { getStars } from "@/lib/helper";
 import { Anime } from "@/models/anime";
-import Image from 'next/image';
 import Link from 'next/link'
 
 export default function CrunchyrollCard(props: any) {
     const anime = props.anime as Anime;
     return (
         <Link href={anime.url} target="_blank">
-        <div className="card-crunchyroll" style={{backgroundImage:`url("${anime.imageUrl.replace('480x720','120x180')}")`, backgroundSize:'cover'}}>
-            
-        </div>
+            <div className="card-crunchyroll">
+                <div className="card-crunchyroll-inner">
+                    <div className="card-crunchyroll-front" style={{ backgroundImage: `url("${anime.imageUrl.replace('480x720', '240x360')}")`, backgroundSize: 'cover' }}>
+                        
+                    </div>
+                    <div className="card-crunchyroll-back text-color4" style={{backgroundImage: '../../../../assets/crunchyroll_back.png'}}> 
+                        <div className="card-crunchyroll-header">
+                            {anime.title}
+                            </div>
+                        {anime.tags && (
+                            <div className="card-crunchyroll-bottom card-crunchyroll-bottom-buttons">
+                            {anime.tags.map(x => {return (<div className="card-crunchyroll-tag-button">{x}</div>)})}
+                        </div>
+                        ) }
+                            <div className="card-crunchyroll-bottom card-crunchyroll-bottom-rating" >{getStars(anime.rating)}</div>
+                            <div className="card-crunchyroll-bottom card-crunchyroll-bottom-desc" >{anime.description.slice(0,100)+' ...'}</div>
+                    </div>
+                </div>
+            </div>
         </Link>
     )
 }
